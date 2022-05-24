320e9fca0b967f6335c0348d950e7b579ea42fd3
Mayur Kamaldhari <mayurb.kamaldhari@gmail.com>
2022-05-24 16:55:48 +0530

    Search page changes

--- src/Pages/Search.js
+++ src/Pages/Search.js
@@ -29,6 +29,7 @@ const Search = () => {
   let [udata, setUdata] = useState(JSON.parse(sessionStorage.getItem("userdata")) || {});
   let [searchUsersList, setSearchUsersList] = useState([]);
   let [searchCollectibleList, setSearchCollectibleList] = useState({});
+  let [searchCollectionList, setSearchCollectionList] = useState({});
   const variants = {
     hidden: { opacity: 0 },
     visible: { opacity: 1 },
@@ -125,7 +126,80 @@ const Search = () => {
                     </div>
 
                     <div className="topSellerContent following-profile-topSellerContent">
-                      <div className="row">
+                      <div className="row mt-5 mb-5">
+                        <div className="d-flex col-lg-12 activity ">
+                          {searchCollectionList.length > 0 ?
+                            <>
+                              {searchCollectionList.map((SingleCollection, key) => (
+                                <LiveAuctions
+                                  isCollection={true}
+                                  id={SingleCollection._id}
+                                  Coverimg={artWorkWeek1}
+                                  liked={SingleCollection.like}
+                                  title={SingleCollection.title}
+                                  heartcount={SingleCollection.likes ? SingleCollection.likes : 0}
+                                  User1={topSellerUser1}
+                                  User2={topSellerUser2}
+                                  User3={topSellerUser3}
+                                  WETH="1.2 WETH"
+                                  bid="Highest bid 1/1"
+                                />
+                              ))}
+                            </> : <>
+                              <div className="col-sm-12 d-flex justify-content-center flex-column text-center">
+                                <h3>Not items found</h3>
+                                <span className="color-gray">
+                                    Come back soon or browse the items on our marketplace.
+                                  </span>
+                                <button className="bg-white profile-not-found-browse-btn mt-4 edit-profile w-25">
+                                  Browse marketplace
+                                </button>
+                              </div>
+                            </>
+                          }
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </TabPane>
+                <TabPane tab="Collectibles" key="2">
+                  <div className="topSeller">
+                    <div className="w-100 d-flex justify-content-end">
+                      <button className="profile-activity-filter-mobile d-web-none">
+                        <svg
+                            width="48"
+                            height="48"
+                            viewBox="0 0 48 48"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <rect
+                              x="0.5"
+                              y="0.5"
+                              width="47"
+                              height="47"
+                              rx="23.5"
+                              fill="white"
+                          />
+                          <path
+                              clipRule="evenodd"
+                              d="M16.5 19V20.6667H31.5V19H16.5ZM22.3333 29H25.6667V27.3333H22.3333V29ZM29 24.8333H19V23.1667H29V24.8333Z"
+                              fill="black"
+                          />
+                          <rect
+                              x="0.5"
+                              y="0.5"
+                              width="47"
+                              height="47"
+                              rx="23.5"
+                              stroke="black"
+                          />
+                        </svg>
+                      </button>
+                    </div>
+
+                    <div className="topSellerContent following-profile-topSellerContent">
+                      <div className="row mt-5 mb-5">
                         <div className="d-flex col-lg-12 activity ">
                           {searchCollectibleList.length > 0 ?
                             <>
@@ -145,30 +219,19 @@ const Search = () => {
                                 />
                               ))}
                             </> : <>
-                              <h3>Not items found</h3>
-                              <span className="color-gray">
-                                Come back soon or browse the items on our marketplace.
-                              </span>
-                              <button className="bg-white profile-not-found-browse-btn mt-4 edit-profile w-25">
-                                Browse marketplace
-                              </button>
+                              <div className="col-sm-12 d-flex justify-content-center flex-column text-center">
+                                <h3>Not items found</h3>
+                                <span className="color-gray">
+                                  Come back soon or browse the items on our marketplace.
+                                </span>
+                                <button className="bg-white profile-not-found-browse-btn mt-4 edit-profile w-25">
+                                  Browse marketplace
+                                </button>
+                              </div>
                             </>
                           }
                         </div>
                       </div>
-                    </div>
-                  </div>
-                </TabPane>
-                <TabPane tab="Items" key="2">
-                  <div className="row mt-5 mb-5">
-                    <div className="col-sm-12 d-flex justify-content-center flex-column text-center">
-                      <h3>Not items found</h3>
-                      <span className="color-gray">
-                        Come back soon or browse the items on our marketplace.
-                      </span>
-                      <button className="bg-white profile-not-found-browse-btn mt-4 edit-profile w-25">
-                        Browse marketplace
-                      </button>
                     </div>
                   </div>
                 </TabPane>
@@ -210,7 +273,7 @@ const Search = () => {
                       </div>
 
                       <div className="topSellerContent following-profile-topSellerContent">
-                        <div className="row">
+                        <div className="row mt-5 mb-5">
                           <div className="d-flex col-lg-12 activity">
                             {searchUsersList.length > 0 ?
                               <div className="d-flex col-lg-12 activity ">
