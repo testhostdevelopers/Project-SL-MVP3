28e1b06a9bd715b590698358763d34dc24ce265e
Mayur Kamaldhari <mayurb.kamaldhari@gmail.com>
2022-05-25 14:35:52 +0530

    STAR-155 Set Display Name

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -24,7 +24,6 @@ if (localStorage.getItem("PublicKey")) {
 }
 
 const { TabPane } = Tabs;
-
 const Profile = (props) => {
   // console.log("props.pImage", props.pImage, "sdasadsa");
   var apiToken = sessionStorage.getItem("apiToken");
